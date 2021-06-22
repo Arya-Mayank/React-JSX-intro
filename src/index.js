602c5c6931006a4d8f3e9ef3a17d82ef9c5b7a8e
Arya-Mayank <68819696+Arya-Mayank@users.noreply.github.com>
2021-06-22 16:15:26 +0530

    use js object for inline styling line: 38

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ const fname = "Mayank";
 const lname = "Arya";
 const luckyNumber = 7;
 
+const img = "https://picsum.photos/200";
+
+const customStyle = {
+  color: "pink",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 
@@ -21,24 +29,13 @@ ReactDom.render(
       <li>list item</li>
     </ul>
     <div>
-      <img
-        alt="burger"
-        className="img"
-        src="https://www.sciencenewsforstudents.org/wp-content/uploads/2020/11/1030_teen_brain_food_choices.jpg"
-      ></img>
-      <img
-        alt="pastries"
-        className="img"
-        src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
-      ></img>
-      <img
-        alt="fastfood"
-        className="img"
-        src="https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/foodnavigator.com/article/2020/04/22/coronavirus-and-obesity-doctors-take-aim-at-food-industry-over-poor-diets/10933380-3-eng-GB/Coronavirus-and-obesity-Doctors-take-aim-at-food-industry-over-poor-diets_wrbm_large.jpg"
-      ></img>
+      <img alt="burger" className="img" src={img}></img>
+      <img alt="pastries" className="img" src={img}></img>
+      <img alt="fastfood" className="img" src={img}></img>
     </div>
-    <p>Created by {fname + " " + lname}.</p>
-    <p>Copyright {currentYear}</p>
+    {/* adding inline css using jsx */}
+    <p style={{ color: "blue" }}>Created by {fname + " " + lname}.</p>
+    <p style={customStyle}>Copyright {currentYear}</p>
   </div>,
   document.getElementById("root")
 );
