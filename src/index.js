7ca69e85edf2306a757eb5d8e462c0db1621066a
Arya-Mayank <68819696+Arya-Mayank@users.noreply.github.com>
2021-06-23 16:27:57 +0530

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
 import React from "react";
 import ReactDom from "react-dom";
-import List from "./list";
+import App from "./App";
 
-const fname = "Mayank";
-const lname = "Arya";
-const luckyNumber = 7;
+ReactDom.render(<App />, document.getElementById("root"));
 
-const img = "https://picsum.photos/200";
+// const fname = "Mayank";
+// const lname = "Arya";
+// const luckyNumber = 7;
 
-const customStyle = {
-  color: "pink",
-  fontSize: "20px",
-  border: "1px solid black"
-};
+// const img = "https://picsum.photos/200";
 
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
+// const customStyle = {
+//   color: "pink",
+//   fontSize: "20px",
+//   border: "1px solid black"
+// };
+
+// const currentDate = new Date();
+// const currentYear = currentDate.getFullYear();
 
 // can use this variable inside HTML by using curly braces {}
 
-ReactDom.render(
-  <div>
-    <h1 className="heading" contentEditable="true" spellCheck="false">
-      Hello {fname + " " + lname}!
-    </h1>
-    <ul>
-      <li>My lucky number is {luckyNumber}</li>
-      <li>list item</li>
-      <li>list item</li>
-    </ul>
-    <div>
-      <img alt="burger" className="img" src={img}></img>
-      <img alt="pastries" className="img" src={img}></img>
-      <img alt="fastfood" className="img" src={img}></img>
-    </div>
-    {/* adding inline css using jsx */}
-    <p style={{ color: "blue" }}>Created by {fname + " " + lname}.</p>
-    <p style={customStyle}>Copyright {currentYear}</p>
-    {/* adding a react component */}
-    <List />
-  </div>,
-  document.getElementById("root")
-);
+// ReactDom.render(
+//   <div>
+//     <h1 className="heading" contentEditable="true" spellCheck="false">
+//       Hello {fname + " " + lname}!
+//     </h1>
+//     <ul>
+//       <li>My lucky number is {luckyNumber}</li>
+//       <li>list item</li>
+//       <li>list item</li>
+//     </ul>
+//     <div>
+//       <img alt="burger" className="img" src={img}></img>
+//       <img alt="pastries" className="img" src={img}></img>
+//       <img alt="fastfood" className="img" src={img}></img>
+//     </div>
+//     {/* adding inline css using jsx */}
+//     <p style={{ color: "blue" }}>Created by {fname + " " + lname}.</p>
+//     <p style={customStyle}>Copyright {currentYear}</p>
+//     {/* adding a react component */}
+//     <List />
+//   </div>,
+//   document.getElementById("root")
+// );
